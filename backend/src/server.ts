dbd77432c5cd9c5b4ee1b0fe8b798c86a40fff05
afjal Ali <mdafjalali272@gmail.com>
2026-03-10 20:44:00 +0530

    backend

---
+++ backend/src/server.ts
@@ -0,0 +1,26 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import prisma from "./config/prisma"
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/api", (req, res) => {
+  res.send("Leora API Running")
+})
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
